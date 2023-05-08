8d933424b4b088b509df5a8035cc6fd37e37cefb
Prasanna Rajendran <imprasannarajendran@gmail.com>
2023-05-08 23:59:48 +0530

    Navbar Updated

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrate } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navBar flex">
+      <div className="navBarOne flex">
+        <div>
+          <SiConsul />
+        </div>
+
+        <div className="none flex">
+          <li className="flex">
+            <BsPhoneVibrate />
+            Support
+          </li>
+          <li className="flex">
+            <AiOutlineGlobal />
+            Languages
+          </li>
+        </div>
+
+        <div className="atb flex">
+          <span>Sign In</span>
+          <span>Sign Up</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
